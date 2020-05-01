4c00441f9e5d3cd3c005a948b35ec5e9e293124e
ip-wataruabe <abe_wataru@interprism.cp.jp>
2020-05-01 16:36:22 +0900

    回答者が最初に扉を選ぶ動きを実装

--- src/shops/Answerer.java
+++ src/shops/Answerer.java
@@ -1,5 +1,57 @@
 package shops;
 
-public class Answerer {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
 
+public class Answerer {
+	private ChoiceMode mode;
+
+	public enum ChoiceMode{
+		auto,
+		manual
+	}
+
+	public Answerer (ChoiceMode mode) {
+		this.mode = mode;
+	}
+
+	public Integer makeTheFirstAnswer(Map<Integer, String> choices) {
+		Integer answer;
+		if (mode == ChoiceMode.manual) {
+			answer = makeAManualChoice(choices);
+		} else {
+			answer = makeAnAutoChoice(choices);
+		}
+
+		return answer;
+	}
+
+	private Integer makeAManualChoice(Map<Integer, String> choices) {
+		Integer answer;
+		Scanner scan = new Scanner(System.in);
+
+		do {
+			System.out.println("Select your answer");
+			choices.forEach((Integer index, String name) -> {
+				System.out.print(index.toString() + " (" + name + ")");
+			});
+			String input = scan.nextLine();
+
+			answer = Integer.valueOf(input);
+		} while (choices.containsKey(answer));
+
+		return answer;
+	}
+
+	private Integer makeAnAutoChoice(Map<Integer, String> choices) {
+		List<Integer> choicesList = new ArrayList<>(choices.keySet());
+
+		Random dice = new Random();
+		int choiceIndex = dice.nextInt(choicesList.size());
+
+		return choicesList.get(choiceIndex);
+	}
 }
--- src/street/FirstAvenue.java
+++ src/street/FirstAvenue.java
@@ -1,7 +1,32 @@
 package street;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import shops.Answerer;
+import shops.Answerer.ChoiceMode;
+import shops.QuizMaster;
+
 public class FirstAvenue {
+	static List<String> choicesOnProgram = Arrays.asList("Left", "Center", "Right");
+
 	public static void main(String[] args) {
 		System.out.println("Hello, Mr.Hall");
+
+		Map<Integer, String> choices = new HashMap<Integer, String>();
+
+		int index = 0;
+		for(String nameOfChoice: choicesOnProgram) {
+			choices.put(index, nameOfChoice);
+			index++;
+		}
+
+		QuizMaster hall = new QuizMaster();
+		hall.lineUpDoors(choices.size());
+
+		Answerer nameless = new Answerer(ChoiceMode.manual);
+		Integer theFirstChoice = nameless.makeTheFirstAnswer(choices);
 	}
 }
