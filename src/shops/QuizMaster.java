7d6dce9ee5ab400f2c6ae4b759213991dc4af754
ip-wataruabe <abe_wataru@interprism.cp.jp>
2020-05-01 15:17:40 +0900

    司会者が3つの扉のうち無作為に選んだ1つに宝物を隠す動きを実装

--- src/shops/QuizMaster.java
+++ src/shops/QuizMaster.java
@@ -1,5 +1,25 @@
 package shops;
 
-public class QuizMaster {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
+public class QuizMaster {
+	private Map<Integer, Door> doors;
+
+	public void lineUpDoors(int numberOfAlternatives) {
+		doors = new HashMap<Integer, Door>();
+		for(int i = 0; i < numberOfAlternatives; i++) {
+			doors.put(new Integer(numberOfAlternatives) ,new Door());
+		}
+
+		int coiceOfRightAnswer = getRandomOneCoiceOf(numberOfAlternatives);
+
+		doors.get(coiceOfRightAnswer).hideTheTreasure();
+	}
+
+	private int getRandomOneCoiceOf(int numberOfAlternatives) {
+		Random dice = new Random();
+		return dice.nextInt(numberOfAlternatives);
+	}
 }
